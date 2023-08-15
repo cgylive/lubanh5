173c3e89680a263160aec17fa2609c50776efcb3
cgylive <ghqcgy1234@163.com>
2023-08-15 16:14:47 +0800

    翻页问题修复

--- front-end/h5/src/components/core/store/modules/questionbanks.js
+++ front-end/h5/src/components/core/store/modules/questionbanks.js
@@ -2,34 +2,32 @@ import Vue from 'vue'
 import strapi from '@/utils/strapi'
 import { AxiosWrapper } from '@/utils/http.js'
 export const actions = {
-    fetchQuestionbanks ({ commit, dispatch, state }) {
-        return new AxiosWrapper({
-          dispatch,
-          commit,
-          name: 'editor/setQuestionbanks',
-          loading_name: 'fetchQuestionbanks_loading',
-          successMsg: '获取数据源成功',
-          customRequest: strapi.getEntries.bind(strapi)
-        }).get('questionbanks')
-      },
-      fetchImageText ({ commit, dispatch, state }) {
-        debugger
-        return new AxiosWrapper({
-          dispatch,
-          commit,
-          name: 'editor/setImageText',
-          loading_name: 'fetchQuestionbanks_loading',
-          successMsg: '获取数据源成功',
-          customRequest: strapi.getEntries.bind(strapi)
-        }).get('questionbanks',{type:'imagetext'})
-      },
+  fetchQuestionbanks({ commit, dispatch, state }) {
+    return new AxiosWrapper({
+      dispatch,
+      commit,
+      name: 'editor/setQuestionbanks',
+      loading_name: 'fetchQuestionbanks_loading',
+      successMsg: '获取数据源成功',
+      customRequest: strapi.getEntries.bind(strapi)
+    }).get('questionbanks')
+  },
+  fetchImageText({ commit, dispatch, state }) {
+    return new AxiosWrapper({
+      dispatch,
+      commit,
+      name: 'editor/setImageText',
+      loading_name: 'fetchQuestionbanks_loading',
+      successMsg: '获取数据源成功',
+      customRequest: strapi.getEntries.bind(strapi)
+    }).get('questionbanks', { type: 'imagetext' })
+  }
 }
 export const mutations = {
-    setQuestionbanks (state, questionbanks) {
-      state.questionbanks = questionbanks.value.data
-    },
-    setImageText(state, imagetext){
-      debugger
-      state.imagetext = imagetext.value.data
-    }
+  setQuestionbanks(state, questionbanks) {
+    state.questionbanks = questionbanks.value.data
+  },
+  setImageText(state, imagetext) {
+    state.imagetext = imagetext.value.data
+  }
 }
